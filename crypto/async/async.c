87204d1fd22f5e1f5d01641961a6c12149870994
Tianjia Zhang <tianjia.zhang@linux.alibaba.com>
2020-03-27 10:37:13 +0800

    ASYNC: Fixes for nested job creation
    In such a scenario, calling ASYNC_start_job() in a job again to create
    a new job will cause the program to crash. This scenario is common in
    coroutines, such as a web server, a coroutine listener, and a new client,
    a new coroutine service, after accept() is created.
    
    The current design of ASYNC does not support this scenario. Each coroutine
    eventually falls back to the dispatcher. There are multiple coroutines,
    and there is only one context for saving the coroutine exit, which is the
    dispatcher. At this time, the context will be Being covered, this also
    determines that only one coroutine can be running at the same time. And it
    is dispatched by the dispatcher, which limits the application scenario
    of ASYNC to a certain extent. In the traditional sense of coroutines,
    dispatching is done by the users themselves, and there is no need for a
    so-called dispatching center, dispatcher.
    
    This patch solves this problem. For each coroutine, a context called back
    is used for exit or ASYNC_job_pause(), so the dispatcher is not needed.
    This patch does not change the ASYNC API.
    
    Signed-off-by: Tianjia Zhang <tianjia.zhang@linux.alibaba.com>

--- crypto/async/async.c
+++ crypto/async/async.c
@@ -43,7 +43,7 @@ static async_ctx *async_ctx_new(void)
         goto err;
     }
 
-    async_fibre_init_dispatcher(&nctx->dispatcher);
+    async_fibre_init_dispatcher(nctx);
     nctx->currjob = NULL;
     nctx->blocked = 0;
     if (!CRYPTO_THREAD_set_local(&ctxkey, nctx))
@@ -153,8 +153,7 @@ void async_start_func(void)
 
         /* Stop the job */
         job->status = ASYNC_JOB_STOPPING;
-        if (!async_fibre_swapcontext(&job->fibrectx,
-                                     &ctx->dispatcher, 1)) {
+        if (!async_fibre_swapcontext(&job->fibrectx, &job->back, 1)) {
             /*
              * Should not happen. Getting here will close the thread...can't do
              * much about it
@@ -168,6 +167,7 @@ int ASYNC_start_job(ASYNC_JOB **job, ASYNC_WAIT_CTX *wctx, int *ret,
                     int (*func)(void *), void *args, size_t size)
 {
     async_ctx *ctx;
+    ASYNC_JOB *new;
 
     if (!OPENSSL_init_crypto(OPENSSL_INIT_ASYNC, NULL))
         return ASYNC_ERR;
@@ -178,16 +178,14 @@ int ASYNC_start_job(ASYNC_JOB **job, ASYNC_WAIT_CTX *wctx, int *ret,
     if (ctx == NULL)
         return ASYNC_ERR;
 
-    if (*job)
-        ctx->currjob = *job;
-
     for (;;) {
-        if (ctx->currjob != NULL) {
+        if (*job) {
+            ctx->currjob = *job;
             if (ctx->currjob->status == ASYNC_JOB_STOPPING) {
                 *ret = ctx->currjob->ret;
                 ctx->currjob->waitctx = NULL;
                 async_release_job(ctx->currjob);
-                ctx->currjob = NULL;
+                ctx->currjob = ctx->currjob->prevjob;
                 *job = NULL;
                 return ASYNC_FINISH;
             }
@@ -195,50 +193,49 @@ int ASYNC_start_job(ASYNC_JOB **job, ASYNC_WAIT_CTX *wctx, int *ret,
             if (ctx->currjob->status == ASYNC_JOB_PAUSING) {
                 *job = ctx->currjob;
                 ctx->currjob->status = ASYNC_JOB_PAUSED;
-                ctx->currjob = NULL;
+                ctx->currjob = ctx->currjob->prevjob;
                 return ASYNC_PAUSE;
             }
 
             if (ctx->currjob->status == ASYNC_JOB_PAUSED) {
+                if (*job != ctx->currjob)
+                    (*job)->prevjob = ctx->currjob;
                 ctx->currjob = *job;
+                ctx->currjob->status = ASYNC_JOB_RUNNING;
                 /* Resume previous job */
-                if (!async_fibre_swapcontext(&ctx->dispatcher,
+                if (!async_fibre_swapcontext(&ctx->currjob->back,
                         &ctx->currjob->fibrectx, 1)) {
                     ASYNCerr(ASYNC_F_ASYNC_START_JOB,
                              ASYNC_R_FAILED_TO_SWAP_CONTEXT);
                     goto err;
                 }
+                ctx->currjob = (*job)->prevjob;
                 continue;
             }
-
-            /* Should not happen */
-            ASYNCerr(ASYNC_F_ASYNC_START_JOB, ERR_R_INTERNAL_ERROR);
-            async_release_job(ctx->currjob);
-            ctx->currjob = NULL;
-            *job = NULL;
-            return ASYNC_ERR;
         }
 
         /* Start a new job */
-        if ((ctx->currjob = async_get_pool_job()) == NULL)
+        if ((new = async_get_pool_job()) == NULL)
             return ASYNC_NO_JOBS;
 
         if (args != NULL) {
-            ctx->currjob->funcargs = OPENSSL_malloc(size);
-            if (ctx->currjob->funcargs == NULL) {
+            new->funcargs = OPENSSL_malloc(size);
+            if (new->funcargs == NULL) {
                 ASYNCerr(ASYNC_F_ASYNC_START_JOB, ERR_R_MALLOC_FAILURE);
-                async_release_job(ctx->currjob);
-                ctx->currjob = NULL;
+                async_release_job(new);
                 return ASYNC_ERR;
             }
-            memcpy(ctx->currjob->funcargs, args, size);
+            memcpy(new->funcargs, args, size);
         } else {
-            ctx->currjob->funcargs = NULL;
+            new->funcargs = NULL;
         }
 
-        ctx->currjob->func = func;
-        ctx->currjob->waitctx = wctx;
-        if (!async_fibre_swapcontext(&ctx->dispatcher,
+        new->func = func;
+        new->waitctx = wctx;
+        new->prevjob = ctx->currjob;
+        ctx->currjob = new;
+        *job = new;
+        if (!async_fibre_swapcontext(&ctx->currjob->back,
                 &ctx->currjob->fibrectx, 1)) {
             ASYNCerr(ASYNC_F_ASYNC_START_JOB, ASYNC_R_FAILED_TO_SWAP_CONTEXT);
             goto err;
@@ -270,8 +267,7 @@ int ASYNC_pause_job(void)
     job = ctx->currjob;
     job->status = ASYNC_JOB_PAUSING;
 
-    if (!async_fibre_swapcontext(&job->fibrectx,
-                                 &ctx->dispatcher, 1)) {
+    if (!async_fibre_swapcontext(&job->fibrectx, &job->back, 1)) {
         ASYNCerr(ASYNC_F_ASYNC_PAUSE_JOB, ASYNC_R_FAILED_TO_SWAP_CONTEXT);
         return 0;
     }
