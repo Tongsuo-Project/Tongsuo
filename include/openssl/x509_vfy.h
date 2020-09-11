8497fa1294ff91a97885ce550553fdc0bc038c4b
Tomas Mraz <tmraz@fedoraproject.org>
2020-09-11 09:09:29 +0200

    Disallow certs with explicit curve in verification chain
    The check is applied only with X509_V_FLAG_X509_STRICT.
    
    Fixes #12139
    
    Reviewed-by: David von Oheimb <david.von.oheimb@siemens.com>
    Reviewed-by: Nicola Tuveri <nic.tuv@gmail.com>
    (Merged from https://github.com/openssl/openssl/pull/12909)
    
    (cherry picked from commit fdcddd9357fcda1f0507fda0307d94e8244f2b51)
    [Yilin: rebase babassl master]
    
    Signed-off-by: YiLin.Li <YiLin.Li@linux.alibaba.com>

--- include/openssl/x509_vfy.h
+++ include/openssl/x509_vfy.h
@@ -184,6 +184,7 @@ void X509_STORE_CTX_set_depth(X509_STORE_CTX *ctx, int depth);
 # define         X509_V_ERR_OCSP_VERIFY_NEEDED                   73  /* Need OCSP verification */
 # define         X509_V_ERR_OCSP_VERIFY_FAILED                   74  /* Couldn't verify cert through OCSP */
 # define         X509_V_ERR_OCSP_CERT_UNKNOWN                    75  /* Certificate wasn't recognized by the OCSP responder */
+# define         X509_V_ERR_EC_KEY_EXPLICIT_PARAMS               79
 
 /* Certificate verify flags */
 
