35ebe390cd85a2d1a277451885136d53eb8be2ff
Paul Yang <kaishen.yy@antfin.com>
2021-06-08 15:22:01 +0800

    Add Chinese ZUC stream cipher
    ZUC is a stream cipher standardized originally by Chinese SCA (State
    Cryptography Administration) in GM/T 0001.1-2012 - GM/T 0001.3-2012.
    
    ZUC is also standardized by 3GPP as an encryption and integrity method
    for using in LTE environment, naming 128-EEA3 and 128-EIA3 respectively.
    
    This commit contains the keystream generation logic of ZUC, which is the
    common part of EEA3 and EIA3. This commit also implements of 3GPP 128-EEA3
    stream cipher, the one that is based on ZUC.
    
    Note: 3GPP 128-EIA3 has not been included in this commit.

--- include/openssl/evp.h
+++ include/openssl/evp.h
@@ -942,6 +942,10 @@ const EVP_CIPHER *EVP_sm4_gcm(void);
 const EVP_CIPHER *EVP_sm4_ccm(void);
 # endif
 
+# ifndef OPENSSL_NO_ZUC
+const EVP_CIPHER *EVP_eea3(void);
+# endif
+
 # if OPENSSL_API_COMPAT < 0x10100000L
 #  define OPENSSL_add_all_algorithms_conf() \
     OPENSSL_init_crypto(OPENSSL_INIT_ADD_ALL_CIPHERS \
