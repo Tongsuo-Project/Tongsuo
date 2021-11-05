ec82ca9e2b622e76b1820ff59600dca0a0d475f5
YiLin.Li <YiLin.Li@linux.alibaba.com>
2021-11-05 11:35:37 +0800

    include: add the date in the OPENSSL_VERSION_TEXT
    openssl version command will display the OPENSSL_VERSION_TEXT. So
    it is neccessary to display the date of openssl 1.1.1h.
    
    Signed-off-by: YiLin.Li <YiLin.Li@linux.alibaba.com>

--- include/openssl/opensslv.h
+++ include/openssl/opensslv.h
@@ -40,7 +40,7 @@ extern "C" {
  *  major minor fix final patch/beta)
  */
 # define OPENSSL_VERSION_NUMBER  0x10101080L
-# define OPENSSL_VERSION_TEXT    "OpenSSL 1.1.1h-dev  xx XXX xxxx"
+# define OPENSSL_VERSION_TEXT    "OpenSSL 1.1.1h  22 Sep 2020"
 
 /*-
  * The macros below are to be used for shared library (.so, .dll, ...)
