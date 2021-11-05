7b0277fea9c4f8c0b6ab610693bcba688638b5fb
YiLin.Li <YiLin.Li@linux.alibaba.com>
2021-11-05 19:42:10 +0800

    rpm: add the RPM spec to install babassl into standard directory to replace openssl
    provide shangmi rpm spec and readme in the patch to help users
    to install babassl into standard directory to replace openssl.
    
    the shangmi rpm spec and scripts based on redhat openssl 1.1.1 spec
    and tested successfully in anolis ShangMi image.
    
    Signed-off-by: YiLin.Li <YiLin.Li@linux.alibaba.com>

--- apps/version.c
+++ apps/version.c
@@ -7,6 +7,9 @@
  * https://www.openssl.org/source/license.html
  */
 
+/* We need to use some deprecated APIs */
+#define OPENSSL_SUPPRESS_DEPRECATED
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
