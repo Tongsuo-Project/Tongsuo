2111825d7e727a809d5912419820f4d75de53283
Chenglong Zhang <auvkone@gmail.com>
2021-11-25 16:21:06 +0800

    Fix speed, use OPENSSL_free instead of free
    Reviewed-by: Tomas Mraz <tomas@openssl.org>
    Reviewed-by: Paul Dale <pauli@openssl.org>
    (Merged from https://github.com/openssl/openssl/pull/17132)

--- apps/speed.c
+++ apps/speed.c
@@ -3826,7 +3826,7 @@ static int do_multi(int multi, int size_num)
             close(fd[1]);
             mr = 1;
             usertime = 0;
-            free(fds);
+            OPENSSL_free(fds);
             return 0;
         }
         printf("Forked child %d\n", n);
@@ -3956,7 +3956,7 @@ static int do_multi(int multi, int size_num)
 
         fclose(f);
     }
-    free(fds);
+    OPENSSL_free(fds);
     return 1;
 }
 #endif
