de6f7d85a690822d894b9ba2b640dfec40256f25
Paul Yang <kaishen.yy@antfin.com>
2022-02-28 10:22:03 +0800

    Move to next version (8.4.0)

--- include/openssl/opensslv.h
+++ include/openssl/opensslv.h
@@ -108,8 +108,8 @@ extern "C" {
  * 1.9.5          0x1090500f
  */
 
-# define BABASSL_VERSION_NUMBER  0x8030000fL
-# define BABASSL_VERSION_TEXT    "BabaSSL 8.3.0"
+# define BABASSL_VERSION_NUMBER  0x80400000L
+# define BABASSL_VERSION_TEXT    "BabaSSL 8.4.0-dev"
 
 #ifdef  __cplusplus
 }
