a6b0681aa61929288320279250037b0c46270878
Jin Jiu <zuxi.wzx@alibaba-inc.com>
2022-06-24 16:54:41 +0800

    Add default provider support for SM4-CCM

--- providers/defltprov.c
+++ providers/defltprov.c
@@ -236,6 +236,7 @@ static const OSSL_ALGORITHM_CAPABLE deflt_ciphers[] = {
     ALG(PROV_NAMES_SM4_OFB, ossl_sm4128ofb128_functions),
     ALG(PROV_NAMES_SM4_CFB, ossl_sm4128cfb128_functions),
     ALG(PROV_NAMES_SM4_GCM, ossl_sm4128gcm_functions),
+    ALG(PROV_NAMES_SM4_CCM, ossl_sm4128ccm_functions),
 #endif /* OPENSSL_NO_SM4 */
 #ifndef OPENSSL_NO_CHACHA
     ALG(PROV_NAMES_ChaCha20, ossl_chacha20_functions),
