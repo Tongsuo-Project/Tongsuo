3d74a79dda26e4ea939bcf07c27daedc332e9a06
Tirupatigopi Ravulapalli <ravulapallix.tirupatigopi@intel.com>
2025-04-07 12:03:18 +0000

    Fix for SM4-CBC failure
    Signed-off-by: Tirupatigopi Ravulapalli <ravulapallix.tirupatigopi@intel.com>

--- apps/speed.c
+++ apps/speed.c
@@ -3547,7 +3547,7 @@ int speed_main(int argc, char **argv)
                     EVP_CIPHER_CTX_rand_key(loopargs[k].ctx, loopargs[k].key);
 		    if (!ae_mode) {
                         if (!EVP_CipherInit_ex(loopargs[k].ctx, NULL, NULL,
-                                               loopargs[k].key, NULL, -1)) {
+                                               loopargs[k].key, iv, -1)) {
                             BIO_printf(bio_err, "\nFailed to set the key\n");
                             dofail();
                             exit(1);
