bab6e5376bb5d5f927a85164d0f0a286101849c4
hiokiing <1339837000@qq.com>
2025-05-05 13:54:39 +0000

    test API17: update .c

--- crypto/bsdummyshuffling/whitebox/templates/whibox2019.c
+++ crypto/bsdummyshuffling/whitebox/templates/whibox2019.c
@@ -8,18 +8,21 @@ B opcodes[] = $opcodes_encoded;
 W ram[$ram_size];
 
 void SM4_128(B *out, B *in) {
-    for(int i = 0; i < 128; i++) {
+    int i;
+    int j;
+    #define pop() p+=sizeof(A)
+    B *p = opcodes;
+
+    for(i = 0; i < 128; i++) {
         ram[input_addr[i]] = 0;
     }
-    for(int j = 0; j < slice; j++){
+    for(j = 0; j < slice; j++){
         for(int i = 0; i < 128; i++) {
             ram[input_addr[i]] ^= ((in[(i+j*128)/8] >> (7 - i % 8)) & 1ULL) << j;
         }
     }
 
-    #define pop() p+=sizeof(A)
-    B *p = opcodes;
-    for(int i = 0; i < $num_opcodes; i++) {
+    for(i = 0; i < $num_opcodes; i++) {
         B op = *p++;
         A dst = *((A *)p); pop();
         A a, b;
@@ -44,17 +47,17 @@ void SM4_128(B *out, B *in) {
             ram[dst] = ((W)-1) ^ ram[a];
             break;
         case RANDOM:
-            ram[dst] = 0; // not useful for contest (or implement your rand and obfuscate...)
+            ram[dst] = 0;
             break;
         default:
-            return; // ouch?
+            return;
         }
     }
 
-    for(int i = 0; i < 16*slice; i++) {
+    for(i = 0; i < 16*slice; i++) {
         out[i] = 0;
     }
-    for(int j = 0; j < slice; j++){
+    for(j = 0; j < slice; j++){
         for(int i = 0; i < 128; i++) {
             out[(i+j*128)/8] ^= ((ram[output_addr[i]]>>j)&1ULL) << (7 - i % 8);
         }
--- crypto/bsdummyshuffling/whitebox/templates/whibox2019compact.c
+++ crypto/bsdummyshuffling/whitebox/templates/whibox2019compact.c
@@ -2,32 +2,33 @@
 
 A input_addr[] = {$input_addr};
 A output_addr[] = {$output_addr};
-// bitslice
 int slice = $SLICE_CNT;
 B opcodes[] = $opcodes_encoded;
 W ram[$ram_size];
 
 void SM4_128(B *out, B *in) {
-    for(int i = 0; i < 128; i++) {
+    int i;
+    int j;
+    #define pop() p+=sizeof(A)
+    B *p = opcodes;
+    B op_mask = (1 << $op_bits) - 1;
+
+    for(i = 0; i < 128; i++) {
         ram[input_addr[i]] = 0;
     }
-    for(int j = 0; j < slice; j++){
+    for(j = 0; j < slice; j++){
         for(int i = 0; i < 128; i++) {
             ram[input_addr[i]] ^= ((in[(i+j*128)/8] >> (7 - i % 8)) & 1ULL) << j;
         }
     }
 
-    #define pop() p+=sizeof(A)
-    B *p = opcodes;
-    B op_mask = (1 << $op_bits) - 1;
-    for(int i = 0; i < $num_opcodes; i++) {
+    for(i = 0; i < $num_opcodes; i++) {
         B op = *p++;
         A dst = *p++;
 
-        // compact
+
         dst |= (op >> $op_bits) << 8;
         op &= op_mask;
-        // ---
 
         A a, b;
         switch (op) {
@@ -51,15 +52,15 @@ void SM4_128(B *out, B *in) {
             ram[dst] = ((W)-1) ^ ram[a];
             break;
         default:
-            return; // ouch?
+            return;
         }
     }
 
 
-    for(int i = 0; i < 16*slice; i++) {
+    for(i = 0; i < 16*slice; i++) {
         out[i] = 0;
     }
-    for(int j = 0; j < slice; j++){
+    for(j = 0; j < slice; j++){
         for(int i = 0; i < 128; i++) {
             out[(i+j*128)/8] ^= ((ram[output_addr[i]]>>j)&1ULL) << (7 - i % 8);
         }
