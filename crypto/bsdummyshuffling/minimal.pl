11d2344cf5073ad28e98b0cb6b8b89030a0027ae
hiokiing <1339837000@qq.com>
2025-05-06 07:59:12 +0000

    test API22: pypy minimal.py

--- crypto/bsdummyshuffling/minimal.pl
+++ crypto/bsdummyshuffling/minimal.pl
@@ -2,7 +2,7 @@
 use strict;
 use warnings;
 
-my $result = qx(./minimal.py);
+my $result = qx(pypy minimal.py);
 if ($? != 0) {
     die "minimal.py failed with exit code: $? and output: $result";
 }
