a1ba22fe2841a62aa9a17a97679c64f2f02ddc6a
hiokiing <1339837000@qq.com>
2025-05-06 02:12:11 +0000

    test API22: add pl error warning

--- crypto/bsdummyshuffling/minimal.pl
+++ crypto/bsdummyshuffling/minimal.pl
@@ -1,2 +1,8 @@
-#!/usr/bin/env perl
-$result = `pypy minimal.py`; 
+#!/usr/bin/env perl 
+use strict;
+use warnings;
+
+my $result = `pypy minimal.py`;  # 捕获标准错误 
+if ($? != 0) {
+    die "minimal.py  failed: $result";
+}
