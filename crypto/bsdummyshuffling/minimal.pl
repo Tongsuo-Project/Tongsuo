b20a4341b9a0444f92bb52d24191dd770b45f673
hiokiing <1339837000@qq.com>
2025-05-06 08:39:59 +0000

    test API23: update py path

--- crypto/bsdummyshuffling/minimal.pl
+++ crypto/bsdummyshuffling/minimal.pl
@@ -1,16 +1,8 @@
 #!/usr/bin/env perl
 use strict;
 use warnings;
-use Cwd qw(getcwd);  # 导入 getcwd 函数
 
-# 获取当前工作目录
-my $current_dir = getcwd();
-
-# 在当前目录前加上 minimal.py 的路径
-my $script_path = "$current_dir/minimal.py";
-
-# 执行 Python 脚本
-my $result = qx(python $script_path);
+my $result = qx(python crypto/bsdummyshuffling/minimal.py);
 if ($? != 0) {
     die "minimal.py failed with exit code: $? and output: $result";
 }
