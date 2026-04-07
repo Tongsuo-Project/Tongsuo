da3014f12a657e98fb50da4002da58946c3e4809
kafei-cy <yaouioh@gmail.com>
2026-04-07 08:47:27 +0800

    Move freeing of an old enc_write_ctx/write_hash to dtls1_clear_sent_buffer
    When we are clearing the sent messages queue we should ensure we free any
    old enc_write_ctx/write_hash that are no longer in use. Previously this
    logic was in dtls1_hm_fragment_free() - but this can end up freeing the
    current enc_write_ctx/write_hash under certain error conditions.
    
    (Merged from https://github.com/openssl/openssl/pull/2261)

--- ssl/ssl_lib.c
+++ ssl/ssl_lib.c
@@ -1263,8 +1263,6 @@ void SSL_free(SSL *s)
     SSL_SESSION_free(s->psksession);
     OPENSSL_free(s->psksession_id);
 
-    clear_ciphers(s);
-
     ssl_cert_free(s->cert);
     OPENSSL_free(s->shared_sigalgs);
 #ifndef OPENSSL_NO_DELEGATED_CREDENTIAL
@@ -1317,6 +1315,12 @@ void SSL_free(SSL *s)
 
     if (s->method != NULL)
         s->method->ssl_free(s);
+
+    /*
+     * Must occur after s->method->ssl_free(). The DTLS sent_messages queue
+     * may reference the EVP_CIPHER_CTX/EVP_MD_CTX that are freed here.
+     */
+    clear_ciphers(s);
 
     SSL_CTX_free(s->ctx);
 
