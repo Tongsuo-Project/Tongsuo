27d276d1ef98e13955e8f2aa56ce9c116dcab198
maxlsc <1785272828@qq.com>
2026-05-22 16:10:25 +0800

    Stop a TLSv1.3 server emitting an unsolicited PSK extension
    If we attempt to accept a connection on an SSL object, and the
    application has set an SSL_SESSION on that SSL object then we
    can mistakenly believe that we are resuming and
    emit an unsolicited PSK extension back to the client.
    
    This can especially happen when using SSL_clear() which leaves
    any SSL_SESSION associated with the SSL object.
    
    See
    https://github.com/openssl/openssl/discussions/27563#discussioncomment-13049352
    and
    https://github.com/openssl/openssl/discussions/24567
    
    Merged from openssl/openssl#27584

--- ssl/ssl_sess.c
+++ ssl/ssl_sess.c
@@ -665,6 +665,8 @@ int ssl_get_prev_session(SSL *s, CLIENTHELLO_MSG *hello)
     SSL_TICKET_STATUS r;
 
     if (SSL_IS_TLS13(s)) {
+        SSL_SESSION_free(s->session);
+        s->session = NULL;
         /*
          * By default we will send a new ticket. This can be overridden in the
          * ticket processing.
@@ -677,6 +679,7 @@ int ssl_get_prev_session(SSL *s, CLIENTHELLO_MSG *hello)
                                         hello->pre_proc_exts, NULL, 0))
             return -1;
 
+        /* If we resumed, s->session will now be set */
         ret = s->session;
 # ifndef OPENSSL_NO_SESSION_REUSED_TYPE
         if (ret != NULL)
