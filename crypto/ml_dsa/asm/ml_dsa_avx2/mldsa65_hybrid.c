0dfc4946a7ccb5bdb2a53f2b66e179fc527c78f3
runqing xu <xu_runqing@foxmail.com>
2026-06-04 21:32:07 +0800

    Generate ML-DSA AVX2 wrappers at build time

--- crypto/ml_dsa/asm/ml_dsa_avx2/mldsa65_hybrid.c
+++
@@ -1,4 +0,0 @@
-#define DILITHIUM_MODE 3
-#define ML_DSA_AVX2_NAMESPACE ossl_ml_dsa_65_avx2_
-#include "ml_dsa_avx2_namespace.h"
-#include "hybrid.c"
--- crypto/ml_dsa/asm/ml_dsa_avx2/mldsa87_sign.c
+++
@@ -1,4 +0,0 @@
-#define DILITHIUM_MODE 5
-#define ML_DSA_AVX2_NAMESPACE ossl_ml_dsa_87_avx2_
-#include "ml_dsa_avx2_namespace.h"
-#include "sign.c"
